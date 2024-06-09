4142eb4fbbba69a3a8a5d9b33001c82b03fdb1a4
PeterTikCheung <petermtcheung@gmail.com>
2024-06-10 00:20:05 +0800

    create new coin contract

---
+++ contracts/NEWCToken.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract NEWCToken is ERC20 {
+    uint8 private _decimals;
+
+    constructor() ERC20("NewCoin", "NEWC") {
+        _decimals = 8;
+
+    }
+    function decimals() public view returns (uint8){
+        return _decimals;
+    }
+
+    function mint (address account) public {
+        _mint(account, 1*(10**uint256(_decimals)));
+    }
+
+}
